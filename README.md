fbe493ed391ce5b3abac59cfc3d43d8a42fb5dc5
SerajAgbarya <eg.seraj@gmail.com>
2022-12-16 14:46:22 +0200

    password

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # CDFI project
 
+python3 manage.py createsuperuser
 python3 manage.py migrate && python3 manage.py runserver
--- app/views.py
+++ app/views.py
@@ -41,25 +41,25 @@ def student_signup(request):
         pass1 = request.POST['pass1']
         pass2 = request.POST['pass2']
         
-        if User.objects.filter(username=username):
-            messages.error(request, "Username already exist! Please try some other username.")
-            return redirect('home')
+        # if User.objects.filter(username=username):
+        #     messages.error(request, "Username already exist! Please try some other username.")
+        #     return redirect('home')
         
-        if User.objects.filter(email=email).exists():
-            messages.error(request, "Email Already Registered!!")
-            return redirect('home')
+        # if User.objects.filter(email=email).exists():
+        #     messages.error(request, "Email Already Registered!!")
+        #     return redirect('home')
         
-        if len(username)>20:
-            messages.error(request, "Username must be under 20 charcters!!")
-            return redirect('home')
+        # if len(username)>20:
+        #     messages.error(request, "Username must be under 20 charcters!!")
+        #     return redirect('home')
         
-        if pass1 != pass2:
-            messages.error(request, "Passwords didn't matched!!")
-            return redirect('home')
+        # if pass1 != pass2:
+        #     messages.error(request, "Passwords didn't matched!!")
+        #     return redirect('home')
         
-        if not username.isalnum():
-            messages.error(request, "Username must be Alpha-Numeric!!")
-            return redirect('home')
+        # if not username.isalnum():
+        #     messages.error(request, "Username must be Alpha-Numeric!!")
+        #     return redirect('home')
         
         myuser = User.objects.create_user(username, email, pass1)
         myuser.first_name = fname
