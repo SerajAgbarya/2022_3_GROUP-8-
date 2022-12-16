00d9ef587aadcb8a581f014860c8c7f668b29eab
SerajAgbarya <eg.seraj@gmail.com>
2022-12-16 15:52:09 +0200

    fix sending email issue - secret should be get from me manually

--- app/views.py
+++ app/views.py
@@ -10,6 +10,7 @@ from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 
 from cdfi import settings
 from .signIn.signIn import signin
+from .signIn.signUp import signup
 from .tokens import generate_token
 
 
@@ -25,71 +26,16 @@ def manager_signin(request):
     return signin(request, "manager-signin.html", "home-manager.html")
 
 
+def worker_signin(request):
+    return signin(request, "worker/worker-signin.html", "worker/home-worker.html")
+
+
 def student_signup(request):
-    if request.method == "POST":
-        username = request.POST['username']
-        fname = request.POST['fname']
-        lname = request.POST['lname']
-        email = request.POST['email']
-        pass1 = request.POST['pass1']
-        pass2 = request.POST['pass2']
+    return signup(request, "student/signup.html", "student_signin")
 
-        # if User.objects.filter(username=username):
-        #     messages.error(request, "Username already exist! Please try some other username.")
-        #     return redirect('home')
 
-        # if User.objects.filter(email=email).exists():
-        #     messages.error(request, "Email Already Registered!!")
-        #     return redirect('home')
-
-        # if len(username) > 20:
-        #     messages.error(request, "Username must be under 20 charcters!!")
-        #     return redirect('home')
-
-        # if pass1 != pass2:
-        #     messages.error(request, "Passwords didn't matched!!")
-        #     return redirect('home')
-
-        # if not username.isalnum():
-        #     messages.error(request, "Username must be Alpha-Numeric!!")
-        #     return redirect('home')
-        myuser = User.objects.create_user(username, email, pass1)
-        myuser.first_name = fname
-        myuser.last_name = lname
-        myuser.is_active = False
-        myuser.save()
-        messages.success(request,
-                         "Your Account has been created succesfully!! Please check your email to confirm your email address in order to activate your account.")
-
-        # Welcome Email
-        subject = "Welcome to CDFI system!"
-        message = "Hello " + myuser.first_name + "!! \n" + "Welcome to CDFI system! \nThank you for visiting us\n. We have also sent you a confirmation email, please confirm your email address. \n\nThanking You"
-        from_email = settings.EMAIL_HOST_USER
-        to_list = [myuser.email]
-        send_mail(subject, message, from_email, to_list, fail_silently=True)
-
-        # Email Address Confirmation Email
-        # current_site = get_current_site(request)
-        email_subject = "Confirm your Email @ CDFI Login!!"
-        message2 = render_to_string('email_confirmation.html', {
-            'name': myuser.first_name,
-            'domain': '127.0.0.1:8000',
-            'uid': urlsafe_base64_encode(force_bytes(myuser.pk)),
-            'token': generate_token.make_token(myuser)
-        })
-        email = EmailMessage(
-            email_subject,
-            message2,
-            settings.EMAIL_HOST_USER,
-            [myuser.email],
-        )
-        email.fail_silently = True
-        email.send()
-
-        return redirect('student_signin')
-
-    return render(request, "student/signup.html")
-
+def worker_signup(request):
+    return signup(request, "worker/signup.html", "worker_signin")
 
 def activate(request, uidb64, token):
     try:
@@ -105,5 +51,23 @@ def activate(request, uidb64, token):
         login(request, myuser)
         messages.success(request, "Your Account has been activated!!")
         return redirect('student_signin')
+    else:
+        return render(request, 'activation_failed.html')
+
+
+def activate_worker(request, uidb64, token):
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        myuser = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        myuser = None
+
+    if myuser is not None and generate_token.check_token(myuser, token):
+        myuser.is_active = True
+        # user.profile.signup_confirmation = True
+        myuser.save()
+        login(request, myuser)
+        messages.success(request, "Your Account has been activated!!")
+        return redirect('worker_signin')
     else:
         return render(request, 'activation_failed.html')
