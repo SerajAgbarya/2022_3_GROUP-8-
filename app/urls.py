7ff182597b61c579d3c3d7ddb2a0d6b3aff242a3
SerajAgbarya <eg.seraj@gmail.com>
2023-01-08 22:04:04 +0200

    handle student task

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
+
 from members import views as v1
+from . import views
 
 urlpatterns = [
     path('', views.main, name='home'),
@@ -11,10 +12,14 @@ urlpatterns = [
     path('student-signin', views.student_signin, name='student_signin'),
     path('worker-signup', views.worker_signup, name='worker_signup'),
     path('worker-signin', views.worker_signin, name='worker_signin'),
-    path('scholarship/submit/', views.submit_scholarship, name='submit_scholarship'),
-    path('scholarship/', views.scholarship_form, name='scholarship_form'),
-    path('scholarship/view/', views.scholarship_view, name='scholarship_view'),
-    path('student/home/', views.student_home_page, name='student_home_page'),
+    # student urls
+    path('student/', views.student_home_page, name='student_home_page'),
+    path('student/scholarship/submit/', views.submit_scholarship, name='submit_scholarship'),
+    path('student/scholarship/create/', views.scholarship_form, name='scholarship_form'),
+    path('student/scholarship/', views.scholarship_view, name='scholarship_view'),
+    path('student/scholarship/edit/', views.scholarship_view, name='scholarship_view'),  # TODO handle edit
+    path('student/task/', views.student_task_page, name='student_task'),
+
     path('login_user', v1.login_user, name='login_user'),
 
 ]
