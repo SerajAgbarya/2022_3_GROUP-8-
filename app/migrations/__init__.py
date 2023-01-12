5b8d1e20a0f417abf892efbb914f1b26b1e73f64
SerajAgbarya <120588059+SerajAgbarya@users.noreply.github.com>
2023-01-12 02:30:08 +0200

    handle student scholarship point calculations (#17)

--- app/migrations/__init__.py
+++ app/migrations/__init__.py
@@ -1,20 +1,59 @@
 from django.contrib.auth.models import Group
 
 from app import constants
+from app.constants import NO, YES, BAD, MID, GOOD, BAD_SMALL, GOOD_SMALL, MID_SMALL, NO_SMALL, YES_SMALL
+from app.models import PointsByYear, PointsByAge, PointsByFinancialSituation, PointsByParentWork, PointsBySpecialNeeds, \
+    PointsByTenant
 
 
 def create_group_if_not_exists(group_name):
-    # Check if a group with the given name already exists
     group = Group.objects.filter(name=group_name).first()
     if group is None:
-        # Create the group if it does not exist
         group = Group(name=group_name)
         group.save()
     else:
         print(f'Group with name:{group_name} already exists')
 
 
-# Use the function to create a group
+def populate_default_points_by_year():
+    for year in range(1, 5):
+        PointsByYear.objects.update_or_create(degree_year=year, points=6 - year)
+
+
+def populate_default_points_by_age():
+    PointsByAge.objects.update_or_create(minAge=18, maxAge=20, points=10)
+    PointsByAge.objects.update_or_create(minAge=21, maxAge=25, points=5)
+    PointsByAge.objects.update_or_create(minAge=26, maxAge=100, points=2)
+
+
+def populate_default_points_by_financial_situation():
+    PointsByFinancialSituation.objects.update_or_create(financial_situation=BAD_SMALL, points=10)
+    PointsByFinancialSituation.objects.update_or_create(financial_situation=MID_SMALL, points=5)
+    PointsByFinancialSituation.objects.update_or_create(financial_situation=GOOD_SMALL, points=1)
+
+
+def populate_default_points_by_parent_work():
+    PointsByParentWork.objects.update_or_create(parent_work=NO_SMALL, points=6)
+    PointsByParentWork.objects.update_or_create(parent_work=YES_SMALL, points=2)
+
+
+def populate_default_points_by_special_needs():
+    PointsBySpecialNeeds.objects.update_or_create(special_needs=NO_SMALL, points=3)
+    PointsBySpecialNeeds.objects.update_or_create(special_needs=YES_SMALL, points=10)
+
+
+def populate_default_points_by_tenant():
+    PointsByTenant.objects.update_or_create(tenant=NO_SMALL, points=2)
+    PointsByTenant.objects.update_or_create(tenant=YES_SMALL, points=8)
+
+
 create_group_if_not_exists(constants.STUDENT)
 create_group_if_not_exists(constants.WORKER)
 create_group_if_not_exists(constants.MANAGER)
+
+populate_default_points_by_year()
+populate_default_points_by_age()
+populate_default_points_by_financial_situation()
+populate_default_points_by_parent_work()
+populate_default_points_by_special_needs()
+populate_default_points_by_tenant()
--- app/models.py
+++ app/models.py
@@ -1,24 +1,35 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-
 # Create your models here.
+from app.constants import NO_YES_CHOICES, FINANCIAL_SITUATION_CHOICES, DEGREE_YEAR_CHOICES
+
+
 class ScholarshipRequest(models.Model):
-    DEGREE_YEAR_CHOICES = [(i, str(i)) for i in range(1, 5)]
-    FINANCIAL_SITUATION_CHOICES = [('bad', 'Bad'), ('mid', 'Mid'), ('good', 'Good')]
-    YES_NO_CHOICES = [('yes', 'Yes'), ('no', 'No')]
+    PENDING = 'PENDING'
+    UNDER_REVIEW = 'UNDER_REVIEW'
+    APPROVED = 'APPROVED'
+    REJECTED = 'REJECTED'
+    STATUS_CHOICES = [
+        (PENDING, 'Waiting For Review'),
+        (UNDER_REVIEW, 'Under Review'),
+        (APPROVED, 'Approved'),
+        (REJECTED, 'Rejected'),
+    ]
 
     id = models.AutoField(primary_key=True)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     degree_year = models.PositiveSmallIntegerField(choices=DEGREE_YEAR_CHOICES)
     age = models.PositiveSmallIntegerField()
     financial_situation = models.CharField(max_length=4, choices=FINANCIAL_SITUATION_CHOICES)
-    parent_work = models.CharField(max_length=3, choices=YES_NO_CHOICES)
-    special_needs = models.CharField(max_length=3, choices=YES_NO_CHOICES)
-    tenant = models.CharField(max_length=3, choices=YES_NO_CHOICES)
-    volunteer = models.CharField(max_length=3, choices=YES_NO_CHOICES)
+    parent_work = models.CharField(max_length=3, choices=NO_YES_CHOICES)
+    special_needs = models.CharField(max_length=3, choices=NO_YES_CHOICES)
+    tenant = models.CharField(max_length=3, choices=NO_YES_CHOICES)
+    volunteer = models.CharField(max_length=3, choices=NO_YES_CHOICES)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=PENDING)
+    points = models.IntegerField(default=0)
 
 
 class Task(models.Model):
@@ -42,9 +53,51 @@ class Task(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+
 class VolunteerHours(models.Model):
     id = models.AutoField(primary_key=True)
     user_id = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='hours_added')
     task_id = models.ForeignKey('app.Task', on_delete=models.CASCADE, related_name='task_in_progress')
     date = models.DateField()
     hours = models.IntegerField()
+
+
+class PointsByYear(models.Model):
+    degree_year = models.PositiveSmallIntegerField(choices=DEGREE_YEAR_CHOICES)
+    points = models.IntegerField(default=0)
+
+
+class PointsByAge(models.Model):
+    age = models.PositiveSmallIntegerField()
+    points = models.IntegerField(default=0)
+
+
+class PointsByAge(models.Model):
+    minAge = models.PositiveSmallIntegerField()
+    maxAge = models.PositiveSmallIntegerField()
+    points = models.IntegerField(default=0)
+
+
+class PointsByFinancialSituation(models.Model):
+    financial_situation = models.CharField(max_length=4, choices=FINANCIAL_SITUATION_CHOICES)
+    points = models.IntegerField(default=0)
+
+
+class PointsByParentWork(models.Model):
+    parent_work = models.CharField(max_length=3, choices=NO_YES_CHOICES)
+    points = models.IntegerField(default=0)
+
+
+class PointsBySpecialNeeds(models.Model):
+    special_needs = models.CharField(max_length=3, choices=NO_YES_CHOICES)
+    points = models.IntegerField(default=0)
+
+
+class PointsByTenant(models.Model):
+    tenant = models.CharField(max_length=3, choices=NO_YES_CHOICES)
+    points = models.IntegerField(default=0)
+
+
+class PointsByTenant(models.Model):
+    tenant = models.CharField(max_length=3, choices=NO_YES_CHOICES)
+    points = models.IntegerField(default=0)
