839f8596214f5b17dbc6545604a913cec86c1f4d
SerajAgbarya <eg.seraj@gmail.com>
2023-01-13 02:38:46 +0200

    handle student scholarship point calculations

--- app/scholarship/scholarship.py
+++ app/scholarship/scholarship.py
@@ -2,9 +2,11 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
-from app.constants import STUDENT_LOGIN_PAGE_PATH, YES_SMALL
+from app.constants import STUDENT_LOGIN_PAGE_PATH, YES_SMALL, FINANCIAL_SITUATION_CHOICES, DEGREE_YEAR_CHOICES, \
+    NO_YES_CHOICES, ON_SMALL
 from app.models import ScholarshipRequest, PointsByYear, PointsByAge, PointsByFinancialSituation, PointsByParentWork, \
     PointsBySpecialNeeds, PointsByTenant
+from app.scholarship.scholarship_dao import get_scholarship_request, create_scholarship_request
 
 
 @login_required(login_url=STUDENT_LOGIN_PAGE_PATH)
@@ -12,21 +14,29 @@ def submit_scholarship_reqeust(request):
     current_user = request.user
     if request.method == 'POST':
         form_data = request.POST
+        msg = 'updated'
+        scholar_ship_request = get_scholarship_request(current_user)
+        if scholar_ship_request is None:
+            msg = 'received'
+            f1 = 'volunteer' in form_data
+            f2 = form_data['volunteer'].lower() == ON_SMALL
+            if f1 and f2:
+                scholar_ship_request = create_scholarship_request()
+                scholar_ship_request.volunteer = YES_SMALL
+            else:
+                messages.error(request, f'Your request cant be accomplished, Volunteer is required!')
+                return redirect('../../')
 
-        # Create a new scholarship request object
-        scholar_ship_request = ScholarshipRequest(
-            degree_year=form_data['degree_year'],
-            age=form_data['age'],
-            financial_situation=form_data['financial_situation'],
-            parent_work=form_data['parent_work'],
-            special_needs=form_data['special_needs'],
-            tenant=form_data['tenant'],
-            volunteer=form_data['volunteer']
-        )
+        scholar_ship_request.degree_year = int(form_data['degree_year'])
+        scholar_ship_request.age = form_data['age']
+        scholar_ship_request.financial_situation = form_data['financial_situation']
+        scholar_ship_request.parent_work = form_data['parent_work']
+        scholar_ship_request.special_needs = form_data['special_needs']
+        scholar_ship_request.tenant = form_data['tenant']
         scholar_ship_request.user_id = current_user
         scholar_ship_request.save()
         calculate_points(scholar_ship_request)
-        messages.success(request, 'Your request has been received successfully')
+        messages.success(request, f'Your request has been {msg} successfully')
         return redirect('../../')
     else:
         return scholarship_form(request)
@@ -34,27 +44,30 @@ def submit_scholarship_reqeust(request):
 
 @login_required(login_url=STUDENT_LOGIN_PAGE_PATH)
 def scholarship_form(request):
-    degree_year_choices = [(i, str(i)) for i in range(1, 5)]
-    financial_situation_choices = [('bad', 'Bad'), ('mid', 'Mid'), ('good', 'Good')]
-    yes_no_choices = [('yes', 'Yes'), ('no', 'No')]
-    return render(request, 'student/scholarship-form.html', {
-        'DEGREE_YEAR_CHOICES': degree_year_choices,
-        'FINANCIAL_SITUATION_CHOICES': financial_situation_choices,
-        'YES_NO_CHOICES': yes_no_choices,
-    })
+    if get_scholarship_request(request.user) is None:
+        return render(request, 'student/scholarship-form.html', {
+            'DEGREE_YEAR_CHOICES': DEGREE_YEAR_CHOICES,
+            'FINANCIAL_SITUATION_CHOICES': FINANCIAL_SITUATION_CHOICES,
+            'YES_NO_CHOICES': NO_YES_CHOICES,
+        })
+
+    return scholarship_view(request)
 
 
 @login_required(login_url=STUDENT_LOGIN_PAGE_PATH)
 def scholarship_view(request):
+    if get_scholarship_request(request.user) is None:
+        return scholarship_form(request)
+
     current_user = request.user
-    print('hi')
     requests = ScholarshipRequest.objects.filter(user_id=current_user)
-    print('hi2')
     if len(requests) > 0:
-        return render(request, 'student/scholarship-form-view.html', {'request': requests[0]})
+        return render(request, 'student/scholarship-form-view.html', {'request': requests[0],
+                                                                      'DEGREE_YEAR_CHOICES': DEGREE_YEAR_CHOICES,
+                                                                      'FINANCIAL_SITUATION_CHOICES': FINANCIAL_SITUATION_CHOICES,
+                                                                      'YES_NO_CHOICES': NO_YES_CHOICES})
     else:
         return render(request, 'student/scholarship-form-view.html', {'request': None})
-    # TODO add info message that there is no opne requests . or att all we should not reach this form if no request eists
 
 
 def get_points_by_year(degree_year):
